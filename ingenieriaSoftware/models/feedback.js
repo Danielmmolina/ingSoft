ea5c2ddde060c9f70f176d3e71197839aafc39d0
Danielmmolina <Daniel.molina2001@alumnos.ubiobio.cl>
2023-05-14 21:23:23 -0400

    Conexión del comentario con brigadista

--- ingenieriaSoftware/models/feedback.js
+++ ingenieriaSoftware/models/feedback.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
+
 const Feedbacks = mongoose.model('Feedback', {
     contenido:{
         type: String,
         require: true
     }
+   /* con el comentario y q el brigadista pertenezca a una cuadrilla
+    y esa cuadrilla pertenezca a una practica
+*/
+
 });
 
 module.exports= Feedbacks;
