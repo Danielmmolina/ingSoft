a7c83d4fda54916afcf05d2082fc20999d7bfd43
Pablo García <68538380+pablitoq@users.noreply.github.com>
2023-05-17 01:40:41 -0400

    Export Practicas CSV

--- ingenieriaSoftware/index.js
+++ ingenieriaSoftware/index.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
+const csv = require('json2csv')
+
 
 dotenv.config();
 
 const feedbackRoutes = require('./routes/feedbackRoutes');
 const brigadistaRoutes = require('./routes/brigadistaRoutes');
-
+const practicaRoutes = require('./routes/practicaRoutes');
 //Conexión a base de datos
 
 mongoose.connect(process.env.DB,(error) =>{
@@ -26,7 +28,7 @@ app.use(express.json());
 app.options('*', cors());
 app.use('/api',feedbackRoutes);
 app.use('/api', brigadistaRoutes);
-
+app.use('/api', practicaRoutes);
 
 
 app.listen(process.env.PORT, () => {
---
+++ ingenieriaSoftware/routes/practicaRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const api = express.Router();
+const practicaController = require('../controllers/practicaController');
+
+api.post('/practica', practicaController.createPractica);
+api.get('/getpractica', practicaController.getPractica);
+api.delete('/deletepractica/:fecha', practicaController.deletePractica);
+api.put('/updatepractica/:fecha', practicaController.updatePractica);
+api.get('/exportPractica', practicaController.exportPractica);
+
+module.exports = api;
