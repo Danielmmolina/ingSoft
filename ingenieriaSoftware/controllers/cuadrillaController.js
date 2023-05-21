1828b11313e49ae9b6b850ec849c6d688760a6c5
Pablo García <68538380+pablitoq@users.noreply.github.com>
2023-05-21 00:43:28 -0400

    Validaciones cuadrillaController

--- ingenieriaSoftware/controllers/cuadrillaController.js
+++ ingenieriaSoftware/controllers/cuadrillaController.js
@@ -1,21 +1,44 @@
 const Cuadrilla = require('../models/cuadrilla');
 const Brigadista = require('../models/brigadista');
 
+const createCuadrilla = async (req, res) => {
+  const { nombre, brigadistas, sector } = req.body;
 
-const createCuadrilla = (req, res) => {
-  const {nombre, brigadistas, sector } = req.body;
-  const newFeedback = new Cuadrilla({
-      nombre,
-      brigadistas,
-      sector
+  // Validar el nombre de la cuadrilla
+  if (!nombre || nombre.length < 3) {
+    return res.status(400).send('ERROR: El nombre de la cuadrilla debe tener al menos 3 caracteres');
+  }
+
+  // Verificar si ya existe una cuadrilla con el mismo nombre
+  const existingCuadrilla = await Cuadrilla.findOne({ nombre: nombre });
+  if (existingCuadrilla) {
+    return res.status(400).send(`ERROR: Ya existe una cuadrilla con el nombre ${nombre}`);
+  }
+
+  // Validar el número de brigadistas
+  if (!brigadistas || brigadistas.length < 1 || brigadistas.length > 50) {
+    return res.status(400).send('ERROR: Debe añadirse al menos un brigadista y máximo 50');
+  }
+
+  // Verificar si los brigadistas están en la base de datos
+  for (const rut of brigadistas) {
+    const brigadista = await Brigadista.findOne({ rut: rut });
+    if (!brigadista) {
+      // El brigadista no está en la base de datos
+      return res.status(400).send(`ERROR: El brigadista con RUT ${rut} no existe en la base de datos`);
+    }
+  }
+
+  // Los brigadistas están en la base de datos, continuar con la creación de la cuadrilla
+  const newCuadrilla = new Cuadrilla({ nombre, brigadistas, sector });
+  newCuadrilla.save((err, Cuadrilla) => {
+    if (err) {
+      return res.status(400).send('ERROR: no se pudo crear el brigadista');
+    }
+    return res.status(201).send(Cuadrilla);
   });
-  newFeedback.save((err, Cuadrilla) => {
-      if(err){
-          return res.status(400).send('ERROR: no se pudo crear el brigadista');
-      }
-      return res.status(201).send(Cuadrilla)
-  });
-}
+};
+
 
 const getCuadrilla = (req, res) => {
     Cuadrilla.find({}, (err, cuadrillas) =>{
