4618ac39eae2a3aeb38482a92b40916a991dc445
Danielmmolina <Daniel.molina2001@alumnos.ubiobio.cl>
2023-07-28 20:10:57 -0400

    borrar comentario

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { ColorMode } from './components/ColorMode'
-import { FormBrigadista } from './components/FormBrigadista'
-import { ListarBrigadista } from './components/ListarBrigadista'
 import { ListarPractica } from './components/ListarPracticas'
-import { CrearPractica } from './components/createPractica'
+
 
 function App() {
 
@@ -11,10 +9,9 @@ function App() {
   return (
     <ChakraProvider>
       <ColorMode/>
-        <FormBrigadista/>
-        <ListarBrigadista/>
+       
         <ListarPractica/>
-        <CrearPractica/>
+        
     </ChakraProvider>
   )
 }
